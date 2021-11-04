bceb999533cdae4fee25551c9822eb20dcda41ae
Manmay <manmaychakarborty@gmail.com>
2021-11-04 18:07:40 +0530

    basic.

---
+++ trial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    a=12;
+    cout<<"size o int"<<sizeof(a)<<endl;
+    float b;
+    cout<<"size o int"<<sizeof(b)<<endl;
+    char c;
+    cout<<"size o int"<<sizeof(c)<<endl;
+    bool d;
+    cout<<"size o int"<<sizeof(d)<<endl;
+}
