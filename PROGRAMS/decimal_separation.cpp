6cc2e88a020d1e6fff13b65e10051ad43c264c10
Manmay <manmaychakarborty@gmail.com>
2023-05-07 12:59:18 +0530

    updated

--- PROGRAMS/decimal_separation.cpp
+++ PROGRAMS/decimal_separation.cpp
@@ -9,7 +9,7 @@ int main()
     cin>>num;
     x=num/1;
     deci=num-x;
-    cout<<"Integer part of the floating number is"<<x;
-    cout<<"Decimal number after separating is"<<deci;
+    cout<<"Integer part of the floating number is "<<x<<endl;
+    cout<<"Decimal number after separating is "<<deci;
     return 0;
 }
