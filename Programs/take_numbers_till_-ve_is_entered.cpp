f26ff10d2001fc432b321fdd6932c4b6452ca121
Manmay <manmaychakarborty@gmail.com>
2023-06-11 11:54:07 +0530

    while loop

---
+++ Programs/take_numbers_till_-ve_is_entered.cpp
@@ -0,0 +1,15 @@
+// WAP that will take keeping numbers until -ve number is entered
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    while (n>0)
+    {
+        cout<<"Enter a number";
+        cin>>n;
+    }
+    return 0;
+}
