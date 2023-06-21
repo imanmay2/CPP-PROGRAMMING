a2585f849a723771906e0b544de976bb61a44944
Manmay <manmaychakarborty@gmail.com>
2023-06-21 07:18:18 +0530

    reversing of a string

--- Programs/Number_system_conversion.cpp
+++ Programs/Number_system_conversion.cpp
@@ -8,3 +8,21 @@
 6. decimal to hexadecimal
 7. ADD 2 BINARY NUMBERS
 */
+
+
+
+#include<iostream>
+using namespace std;
+
+string rev(string s){
+    
+}
+int bin_deci(string bin){
+    int s=0,k=0;
+    for(int i=0;i<bin.size();i++){
+
+    }
+}
+int main(){
+    return 0;
+}
