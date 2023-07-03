f8bd52e214f061167e860efb2d4011c83d419149
Manmay <manmaychakarborty@gmail.com>
2023-07-03 11:49:52 +0530

    challenge to be done by today

---
+++ Programs/longest_arithmetic_subarray.cpp
@@ -0,0 +1,22 @@
+// longest arithmetic subarray.
+// e.g : [10,7,4,6,8,10,11]
+//       output should be 4.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the range of the array";
+    cin>>n;
+    int arr[n];
+    //inserting the element into tehe array.
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Enter the element : ";
+        cin>>arr[i];
+    }
+
+
+    // main code of arithmetic algorithm starts here.
+    
+}
