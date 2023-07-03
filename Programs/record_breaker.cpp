c8915676fcaf49470b0e60b06268973eb54ca28e
Manmay <manmaychakarborty@gmail.com>
2023-07-03 11:49:10 +0530

    challenges to be done by today

---
+++ Programs/record_breaker.cpp
@@ -0,0 +1,30 @@
+// REcord breaking data. 
+// [1,2,0,7,2,0,2,2]
+
+// conditions to be noted is that : 
+// the number of visitors on that day is strictly larger than the number 
+// of visitors on each of the previous days.
+// Either it is the last day, or the number of visitors on the day is strictly larger than the number of visitors 
+// on the followijg day.
+// NOTE : every first dat could be record breaking days.
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the size of the array : ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"Enter the elements : ";
+        cin>>arr[i];
+    }
+
+
+    // main code starts here.
+
+    
+}
