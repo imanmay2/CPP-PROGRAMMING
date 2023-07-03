c1469f41697b27840e5d067e4761442dc202579b
Manmay <manmaychakarborty@gmail.com>
2023-07-03 10:27:26 +0530

    subarrays and subsequence(THEORY)

---
+++ Programs/subarray and subsequence.cpp
@@ -0,0 +1,26 @@
+/*
+SUBARRAY : CONTINUOUS PART OF THE ARRAY
+
+no. of subarrays of an array with n elements = nC2 + n 
+                                             = (n*(n+1))/2
+
+
+
+
+SUNSEQUENCE : A SUBSEQUCNE IS A SEQUENCE THAT CAN BE DERIVED AN ARRAY BY SELECTING 
+            ZERO OR MORE ELEMENTS, WITHOUT CHANGING THE ORDER OF THE REMAINING ELEMENTS.
+
+
+no. of subsequence of an array with n elements = 2^n.
+
+
+
+NOTE:  Every Subarray is a sequence but every subsequence is not a subarray.
+
+*/
+
+
+
+
+
+
