b60c1a31e8acabf5dd38a47c607722fa70c65f85
Manmay <manmaychakarborty@gmail.com>
2023-08-15 10:42:40 +0530

    bug yet to be fixed

--- Programs/sum_of_the_given_subarray.cpp
+++ Programs/sum_of_the_given_subarray.cpp
@@ -9,3 +9,37 @@
 //EXPLAINATION : The sum of elements from 2nd position to 4th position is 12.
 
 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n,f=0;
+    cout<<"Enter the size of the array : ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"Enter the element : ";
+        cin>>arr[i];
+    }
+
+    int S;
+    cout<<"Enter the sum : ";
+    cin>>S;
+    //traversing the array.
+    for(int i=0;i<n;i++){
+        int s=0;
+        for(int j=0;j<i;j++){
+            s=s+arr[j];
+            if(S==s){
+                cout<<"SUM MATCHED !!\n";
+                cout<<i<<j;
+                f=1;
+                break;
+            }
+        }
+    }
+    if(f==0){
+        cout<<"SORRY !! SUM DOESN'T EXIXTS IN THE GIVEN ARRAY!!";
+    }
+    return 0;
+}
