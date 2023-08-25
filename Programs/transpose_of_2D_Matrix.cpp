63a9c06595dbdf1e230034992b43175fbf66d917
Manmay <manmaychakarborty@gmail.com>
2023-08-25 18:43:43 +0530

    bug to be fixed soon

---
+++ Programs/transpose_of_2D_Matrix.cpp
@@ -0,0 +1,34 @@
+// Print the transpose of  a  square matrix.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the order of the square matrix : ";
+    cin>>n;
+    int arr[n][n];
+    int arr1[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<"Enter the element  : ";
+            cin>>arr[i][j];
+        }
+    }
+
+
+    //transposing the matrix.
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            arr1[i][j]=arr[j][i];
+        }
+    }
+
+    //printing the transposed array.
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
