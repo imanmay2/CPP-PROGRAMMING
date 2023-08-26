ae42f1d96c96b58e20f9ab5e46c5f8677121b762
Manmay <manmaychakarborty@gmail.com>
2023-08-26 17:02:28 +0530

    ALGO YET TO BE DEPLOYED

---
+++ Programs/multiplication_of_2D_Matrix.cpp
@@ -0,0 +1,16 @@
+// Print the matrix multiplication of 2 2D arrays of n1Xn2 and n2Xn3.
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c,d;
+    cout<<"Enter the row and coloumn for Matrix1 : ";
+    cin>>a>>b;
+    cout<<"Enter the row and coloumn for Matrix2 : ";
+    cin>>c>>d;
+    if(b!=c){
+        cout<<"Error, Matrix Mulyiplication cannot be done due to the improper values of rows and column.";
+    }
+    
+}
