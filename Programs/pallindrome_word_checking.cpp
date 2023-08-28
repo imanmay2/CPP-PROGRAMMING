5a81e6e019ff317d63c8c0c7240d04dee0734f0b
Manmay <manmaychakarborty@gmail.com>
2023-08-28 18:39:02 +0530

    ALGO/BUG FIXED

--- Programs/pallindrome_word_checking.cpp
+++ Programs/pallindrome_word_checking.cpp
@@ -8,25 +8,23 @@ int main(){
     int n;
     cout<<"Enter the range : ";
     cin>>n;
-    int arr[n+1];      //NOTE : +1 IS ADDED FOR THE LAST/NULL CHARACTER : '\0'
+    char arr[n+1];      //NOTE : +1 IS ADDED FOR THE LAST/NULL CHARACTER : '\0'
     cout<<"Enter a word : ";
-    cin>>arr[n+1];
-    int f=0;
+    cin>>arr;
+    bool f=true;
     for(int i=0;i<n;i++)
     {
-        if(arr[i]==arr[n-1-i]){
-            f++;
+        if(arr[i]!=arr[n-1-i]){
+            f=false;
+            cout<<"WORD IS NOT PALINDROME.";
+            break;
         }
     }
+    if(f==true){
+        cout<<"WORD IS PALINDROME.";
+    }
 
-    cout<<n<<f;
-    if(f==n) {
-        cout<<"WORD ENTERED IS PALLINDROME";
-    }
-    else{
-        cout<<"WORD IS NOT PALLINDROME";
-    }
-    
+
 
     return 0;
 }
