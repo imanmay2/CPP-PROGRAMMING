2cb07d2d312a9c9ed5604e0713f77f5a23b27292
Manmay <manmaychakarborty@gmail.com>
2023-09-05 00:55:27 +0530

    MNC Q CRACKED (FACEBOOK , AMAZON )

--- Programs/find_repeating_smallest_index_element_of_array.cpp
+++ Programs/find_repeating_smallest_index_element_of_array.cpp
@@ -3,4 +3,34 @@
 //	E.g : [1,5,3,4,3,5,6]
 //	 output : 2               (FACEBOOK , AMAZON )
 
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the range of the array : ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"Enter the element : ";
+        cin>>arr[i];
+    }
+
+
+
+    // traversing the array.
+    int ct=0;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            if(arr[i]==arr[j]){
+                ct++;
+                if(ct==2){
+                    cout<<"The First repeating element is : "<<arr[i]<<" And it's position is : "<<i+1;
+                    break;
+            }
+        }
+        
+        }
+    }
+    return 0;
+}
 
