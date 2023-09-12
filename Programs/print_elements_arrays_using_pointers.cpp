cd7eab8f3b67ce601e9cddded8bcc382eae49f5b
Manmay <manmaychakarborty@gmail.com>
2023-09-13 00:06:55 +0530

    array printed successfully,(using pointers)

--- Programs/print_elements_arrays_using_pointers.cpp
+++ Programs/print_elements_arrays_using_pointers.cpp
@@ -3,5 +3,12 @@
 using namespace std;
 int main(){
     int arr[]={10,20,30,40};
-    
+    // cout<<*arr<<endl;  de-referencing the first value of the array.
+    int *ptr =arr;
+    for(int i=0;i<4;i++){
+         cout<<*ptr<<endl;
+        //cout<<*(arr+i)<<endl;   // we may write this statement instead of the previous statement
+        ptr++;
+    }
+    return 0;
 }
