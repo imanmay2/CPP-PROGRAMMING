2ea258b8d7d4961f58ebb3ab17e892393c34ba59
Manmay <manmaychakarborty@gmail.com>
2023-09-12 23:57:48 +0530

    basics of pointers updated

--- Programs/pointers_basics.cpp
+++ Programs/pointers_basics.cpp
@@ -1,10 +1,18 @@
+// BASICS OF POINTERS , ACCESSING THE ADDRESS, DE-REFERENCING .
+
 #include<iostream>
 using namespace std;
 int main(){
     int a=10;
     int *aptr; // declaring a pointer variable for a.
     aptr=&a;
-    cout<<"Adrress of "<<a<<"is : "<<aptr<<endl; // printing the address of the value of a.
+    cout<<"Adrress of "<<a<<"is : "<<aptr<<" "<<aptr+1<<endl; // printing the address of the value of a.
     cout<<"Value of the adrressed value  : "<<*aptr; // de-referencing to get the value from the desired address.
+
     return 0;
 }
+
+
+
+//NOTE : if we are increasing the address of a integer pointer then , each time increasing by +1 , it will be increased by +4.
+//                                             char  pointer then , it will be increased by 1  , on increasing +1 each times.
---
+++ Programs/print_elements_arrays_using_pointers.cpp
@@ -0,0 +1,7 @@
+// WAP THAT WILL ACCESS THE ELEMENTS OF THE ARRAY USING POINTERS.
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={10,20,30,40};
+    
+}
