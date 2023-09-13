a61b5485af317289ed1864a1759f5dfcb1f5fd6b
Manmay <manmaychakarborty@gmail.com>
2023-09-13 09:43:57 +0530

    CONCEPT DEPLOYED

--- Programs/pointers_to_pointers.cpp
+++ Programs/pointers_to_pointers.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 using namespace std;
-int main(){
+int maim(){
     int a=10;
     int *p;
     p=&a;
-    int **q;
-    **q=&p;
     cout<<*p<<endl;
+    int **q=&p;
+    cout<<*q<<endl;
     cout<<**q;
+    return 0;
 }
