5555050f738b01b59e54709724d89ef51acee555
Manmay <manmaychakarborty@gmail.com>
2023-09-18 22:35:24 +0530

    bug yet to be fixed

---
+++ Programs/lowercase_UPPERCASE.cpp
@@ -0,0 +1,19 @@
+// WAP THAT WILL ACCEPT A STRING FROM THE USER AND DISPLAY BOTH THE UPPERCASE AND LOWERCASE .
+
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    string s;
+    cout<<"Enter the string : ";
+    getline(cin,s);
+    cout<<endl;
+    transform(s.begin(),s.end(),s.begin()::toupper());
+    cout<<"THE STRING IN UPPERCASE FORMAT IS : "<<s<<endl;
+    transform(s.begin(),s.end(),s.begin(),::tolower);
+    cout<<"THE STRING IN LOWERCASE FORMAT IS : "<<s<<endl;
+    return 0;
+    
+}
