eb2f712831892e8c4fbceb7f7d431e56a7c73f36
Manmay <manmaychakarborty@gmail.com>
2023-09-19 13:21:53 +0530

    bug fixed(comma missing)

--- Programs/lowercase_UPPERCASE.cpp
+++ Programs/lowercase_UPPERCASE.cpp
@@ -10,7 +10,7 @@ int main(){
     cout<<"Enter the string : ";
     getline(cin,s);
     cout<<endl;
-    transform(s.begin(),s.end(),s.begin()::toupper());
+    transform(s.begin(),s.end(),s.begin(),::toupper);
     cout<<"THE STRING IN UPPERCASE FORMAT IS : "<<s<<endl;
     transform(s.begin(),s.end(),s.begin(),::tolower);
     cout<<"THE STRING IN LOWERCASE FORMAT IS : "<<s<<endl;
