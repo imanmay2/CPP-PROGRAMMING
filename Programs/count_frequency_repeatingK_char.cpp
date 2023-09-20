c6235cf17bf20cfea582fbfcc40f387aa6b9b157
Manmay <manmaychakarborty@gmail.com>
2023-09-20 23:30:03 +0530

    challenge yet to be completed

---
+++ Programs/count_frequency_repeatingK_char.cpp
@@ -0,0 +1,14 @@
+// TAKE INPUT A STRING AND DISPLAY THE MOST OCCURENCE STRING AND IT'S COUNT OF THE FREQUENCY TOO.
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    cout<<"Enter the string : ";
+    getline(cin,str);
+    cout<<endl;     // input from user.
+
+
+}
