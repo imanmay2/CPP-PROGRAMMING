dcc17325a03ffc926e9d412446838919fed0b283
Manmay <manmaychakarborty@gmail.com>
2023-09-21 21:27:49 +0530

    logic yet to be deployed

--- Programs/count_frequency_repeatingK_char.cpp
+++ Programs/count_frequency_repeatingK_char.cpp
@@ -1,28 +1,12 @@
-// TAKE INPUT A STRING AND DISPLAY THE MOST OCCURENCE STRING AND IT'S COUNT OF THE FREQUENCY TOO.
-// example : 'abcdaabc'
+// WAP that will display the count of the most repeating string and the string too.
 
 #include<iostream>
 #include<string>
 #include<algorithm>
-#include<climits>
 using namespace std;
-bool check_str(int n, string arr,string s){
-    for(int i=0;i<n;i++){
-        if(s==arr[i]){
-            return true;
-        }
-    }
-}
 int main(){
+    int arr[26];
+    string alpha="abcdefghijklmnopqrstuvwxyz";
     string str;
-    getline(cin,str);
-    string arr[str.size()];
-    for(int i=0;i<str.size();i++){
-        if(!check_str(str.size(),arr,str[i])){
-            arr[i]=str[i];
-        }
-    }
-
-    cout<<arr;
-    return 0;
+    
 }
