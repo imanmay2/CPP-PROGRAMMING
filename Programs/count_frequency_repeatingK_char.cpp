57793fbf2d83fb9a98258f765eac20a761548bda
Manmay <manmaychakarborty@gmail.com>
2023-09-21 17:32:23 +0530

    logical error...(need to think about algo)

--- Programs/count_frequency_repeatingK_char.cpp
+++ Programs/count_frequency_repeatingK_char.cpp
@@ -1,14 +1,28 @@
 // TAKE INPUT A STRING AND DISPLAY THE MOST OCCURENCE STRING AND IT'S COUNT OF THE FREQUENCY TOO.
-
+// example : 'abcdaabc'
 
 #include<iostream>
 #include<string>
+#include<algorithm>
+#include<climits>
 using namespace std;
+bool check_str(int n, string arr,string s){
+    for(int i=0;i<n;i++){
+        if(s==arr[i]){
+            return true;
+        }
+    }
+}
 int main(){
     string str;
-    cout<<"Enter the string : ";
     getline(cin,str);
-    cout<<endl;     // input from user.
+    string arr[str.size()];
+    for(int i=0;i<str.size();i++){
+        if(!check_str(str.size(),arr,str[i])){
+            arr[i]=str[i];
+        }
+    }
 
-
+    cout<<arr;
+    return 0;
 }
