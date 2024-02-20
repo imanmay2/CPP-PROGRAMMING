3c4b81e18a73d608c341b39edc679076fa79deb6
manmay2 <imanmay2@gmail.com>
2024-02-20 13:24:15 +0530

    logic to be deployed

--- Programs/matrix_multiplication.cpp
+++ Programs/matrix_multiplication.cpp
@@ -1,3 +1,39 @@
-// WAP THAT WILL ACCEPT 2D ARRAY AND DISPLAY THE MULTIPLICATION OF THAT.
+// WAP THAT WILL ACCEPT 2D ARRAYS AND DISPLAY THE MULTIPLICATION OF THAT.
 
 
+#inlcude<iostream>
+using namespace std;
+int main(){
+    int m,n;
+    cout<<"Enter the rows and coloumns for ARRAY_1 : "<<endl;
+    cin>>m>>n;
+    int arr1[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            int num;
+            cout<<"Enter the number : "<<endl;
+            cin>>arr1[i][j];
+        }
+    }
+
+
+     int m_=m;
+     int n_=n;
+     int arr2[m_][n_];
+    for(int i=0;i<m_;i++){
+        for(int j=0;j<n_;j++){
+            int num_;
+            cout<<"Enter the number : "<<endl;
+            cin>>arr2[i][j];
+        }
+    }
+
+    //creating new array, for storing the multiplication of the previous arrays
+
+    int arr_[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            // logic to be deployed over here....
+        }
+    }
+}
