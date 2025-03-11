798675bfcba173649cfcd2b93ab0e05bf3887b48
manmay2 <imanmay2@gmail.com>
2025-03-11 22:28:08 +0530

    ToggleCase of string done

---
+++ Programs/lowercase_uppercase_string.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void toggleCase(string &str){
+    for(int i=0;i<str.length();i++){
+        if(str[i]>='A' && str[i]<='Z'){
+            str[i]=str[i]+32;
+        } else{
+            str[i]=str[i]-32;
+        }
+    }
+}
+
+int main(){
+    string str;
+    cout<<"Enter the string: ";
+    getline(cin,str);
+    cout<<str<<endl;
+    toggleCase(str);
+    cout<<str;
+}
