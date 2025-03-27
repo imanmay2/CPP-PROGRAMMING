083108633975a2565f76b569e49274f792381c2b
manmay2 <imanmay2@gmail.com>
2025-03-27 18:30:09 +0530

    DA1 Question done successfully.

--- Programs/DA_1.cpp
+++ Programs/DA_1.cpp
@@ -13,10 +13,39 @@
 // C B C B H C B C B H
 // C B C B H C B C B H
 // C B C B H C B C B H
-// C B C B H
-
+// C B C B H.
 // Total Cars: 25
 
 // Total Bikes: 20
 
 // Total Heavy Vehicles: 5
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number of slots for parking : ";
+    cin>>n;
+    int ct_h=0,ct_b=0,ct_c=0;
+    for(int i=1;i<=n;i++){
+        if(i%10==0){
+            cout<<"H ";
+            cout<<endl;
+        }
+        else if(i%5==0){
+            cout<<"H ";
+            ct_h++;
+        } else if(i%2==0){
+            cout<<"B ";
+            ct_b++;
+        } else if(i%2!=0){
+            cout<<"C ";
+            ct_c++;
+        }
+    } cout<<endl;
+    cout<<"Total Cars : "<<ct_c<<endl;
+    cout<<"Total Bikes : "<<ct_b<<endl;
+    cout<<"Total Heavy Vehicles: "<<ct_h<<endl;
+    
+}
