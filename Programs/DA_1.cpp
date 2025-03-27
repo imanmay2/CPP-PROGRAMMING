2ac401bfcee61fc1e0dd787d9897c11f8ecd7d15
manmay2 <imanmay2@gmail.com>
2025-03-27 17:55:42 +0530

    DA_1 c++ digital assignment Q

---
+++ Programs/DA_1.cpp
@@ -0,0 +1,22 @@
+// 1.A parking lot has 50 slots arranged in a single row. Assign parking slots as follows:
+// · Vehicles with an odd number are cars (C).
+// · Vehicles with an even number are bikes (B).
+// · Every 5th slot is reserved for heavy vehicles (H).
+
+// Write a program to:
+// 1. Display the parking slot allocation.
+// 2. Count the total number of cars, bikes, and heavy vehicles.
+// Sample Input:
+// Enter the total number of parking slots: 50
+// Slot Allocations:
+// C B C B H C B C B H
+// C B C B H C B C B H
+// C B C B H C B C B H
+// C B C B H C B C B H
+// C B C B H
+
+// Total Cars: 25
+
+// Total Bikes: 20
+
+// Total Heavy Vehicles: 5
