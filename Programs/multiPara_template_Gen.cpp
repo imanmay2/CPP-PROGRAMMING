b6867c3fa0223fc151240e1395b2e5e2bf41ddc6
manmay2 <imanmay2@gmail.com>
2025-04-20 19:03:57 +0530

    Generic Programming

--- Programs/multiPara_template_Gen.cpp
+++ Programs/multiPara_template_Gen.cpp
@@ -1,29 +1,28 @@
-//Multiparameter class Template
+//multipara_template_
+
 
 #include<iostream>
 using namespace std;
 
-template<class t1,class t2>
+template<class T1,class T2>
 class Number{
-    t1 num1;
-    t2 num2;
+    T1 num1;
+    T2 num2;
 
     public:
-    Number(t1 num1,t2 num2){
+    Number(T1 num1,T2 num2){
         this->num1=num1;
         this->num2=num2;
     }
 
-    t1 add(){
+    T1 add(){
         return num1+num2;
     }
 };
 
-
-
 int main(){
-    Number<int,double> n1(2,3.5);
-    Number<double,int> n2(3.5,2);
+    Number<int,double> n1(5,5.5);
+    Number<double,int> n2(6.5,5);
     cout<<n1.add()<<endl;
-    cout<<n2.add()<<endl;
+    cout<<n2.add();
 }
--- Programs/template_function.cpp
+++ Programs/template_function.cpp
@@ -3,10 +3,11 @@
 #include<iostream>
 using namespace std;
 
-template<class t> t add(const t &n1,const t &n2){
-    return n1+n2;
+template<class t,class t1> t add(t const &num1,t1 const &num2){
+    return num1+num2;
 }
 
 int main(){
-    cout<<"Addition is : "<<add(1,4)<<endl;
+    cout<<add(1,3.5)<<endl;
+    cout<<add(3.5,1)<<endl;
 }
