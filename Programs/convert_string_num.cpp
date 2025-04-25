75db1e96b7d0847ad0ecf02f0e7b55abccf15389
manmay2 <imanmay2@gmail.com>
2025-04-26 02:17:51 +0530

    task yet to be completed

--- Programs/convert_string_num.cpp
+++ Programs/convert_string_num.cpp
@@ -2,9 +2,26 @@
 
 #include<iostream>
 #include<string>
+#include<math.h>
 
 using namespace std;
 
+void string_to_num(string str){
+    int num=0;
+    int n=pow(10,str.size());
+    //Number is let's say : 1234.
+    for(int i=0;i<str.length();i++){
+        
+    }
+
+    cout<<"Number is : "<<num;
+}
+
 int main(){
+    string str;
+    cout<<"Enter the string : ";
+    cin>>str;
     
+    string_to_num(str);
+
 }
