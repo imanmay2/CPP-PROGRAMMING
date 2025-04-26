d75a43072b1eee296ca1f2e29f7b67012bf342f0
manmay2 <imanmay2@gmail.com>
2025-04-26 09:21:51 +0530

    String to number conversion done

--- Programs/convert_string_num.cpp
+++ Programs/convert_string_num.cpp
@@ -1,27 +1,33 @@
 //Write a program that will convert string into number.
 
 #include<iostream>
-#include<string>
 #include<math.h>
-
+#include<string>
 using namespace std;
 
-void string_to_num(string str){
-    int num=0;
-    int n=pow(10,str.size());
-    //Number is let's say : 1234.
-    for(int i=0;i<str.length();i++){
-        
-    }
 
-    cout<<"Number is : "<<num;
+
+
+int string_to_num(string str){
+    int num=0;
+    int n=pow(10,str.size()-1);
+    for(int i=0;i<str.size();i++){
+        int dig=str[i]-'0';
+        if(dig<0 || dig>9){
+            cout<<"Invalid Character entered ! "<<endl;
+            return 0;
+        }
+        num=num+(dig*n);
+        n=n/10;
+    }
+    return num;
 }
+
+
 
 int main(){
     string str;
     cout<<"Enter the string : ";
     cin>>str;
-    
-    string_to_num(str);
-
+    cout<<"Your number is : "<<string_to_num(str);
 }
